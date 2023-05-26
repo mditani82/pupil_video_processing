c3e19623f36ffe6c07a509de68acd7df14bd06e4
Mohamad <mditani82@gmail.com>
2023-05-26 13:53:34 -0700

    move files to data directory

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from util.aws import readFileFromS3, processVideo
 
-filename = '1685046783178.mp4'
+filename = '16850467831781.mp4'
 
 downloadFile = readFileFromS3(filename)
 
 if downloadFile['message'] == 'Success':
     # createImagesFromVideo(filename)
-    processVideo('pods', filename, 309, 654, 759 , 1194, False, '0')
+    processVideo('pods', filename, 309, 654, 759 , 1194, False, '1')
 else:
     print(downloadFile['message'])
     pass
--- util/aws.py
+++ util/aws.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 
 bucketName = 'pupilengine'
 directory = f'{os.getcwd()}/downloads'
+imagesDataDirectory  = f'{os.getcwd()}/data/images' 
+labelsDataDirectory = f'{os.getcwd()}/data/labels' 
 res_height = 1024
 
 def readFileFromS3(filename: str):
@@ -74,10 +76,10 @@ def processVideo(name: str, filename: str, xmin, ymin, xmax, ymax, drawPOI, labe
                     
         index += 1
 
-        # if index == 10:
-        #     break
+        if index == 10:
+            break
 
-    splitTrainTest(videoDirectory)
+    splitTrainTest(videoDirectory, filename, labelIndex)
 
 def generateTxTfiles(img, bbox, labelIndex, filename, index, videoDirectory):
 
@@ -114,7 +116,9 @@ def drawBox(img, bbox):
         cv2.rectangle(img, (x,y), ((x+w), (y+w)), (255,0,255), 3,1)
         cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,255), 2)
 
-def splitTrainTest(videoPath):
+def splitTrainTest(videoPath, filename, labelIndex):
+
+    foldersValidation(videoPath, filename, labelIndex)
     
     # getting list of images
     dir_list = os.listdir(videoPath)
@@ -126,27 +130,50 @@ def splitTrainTest(videoPath):
             images.append(name)
 
     # splitting the dataset
-    train_names, test_names = train_test_split(images, test_size=0.3)
+    train_names, test_names = train_test_split(images, test_size=0.3, shuffle=True)
 
-    def batch_move_files(file_list, source_path, destination_path):
+    def batch_move_files(file_list, source_path, destination_path, ext):
         for file in file_list:
-            image = file[:-3] + 'jpg'
-            label = file[:-3] + 'txt'
-            shutil.move(os.path.join(source_path, image), destination_path)
-            shutil.move(os.path.join(source_path, label), destination_path)
+            _name = file[:-3] + ext
+            shutil.move(os.path.join(source_path, _name), destination_path)
         return
+
+    test_dir = videoPath + f'/images/test_{labelIndex}'
+    train_dir = videoPath + f'/images/train_{labelIndex}'
+
+    batch_move_files(train_names, videoPath, train_dir, 'jpg')
+    batch_move_files(test_names, videoPath, test_dir, 'jpg')
+
+    test_dir_labels = videoPath + f'/labels/test_{labelIndex}'
+    train_dir_labels = videoPath + f'/labels/train_{labelIndex}'
+
+    batch_move_files(train_names, videoPath, train_dir_labels, 'txt')
+    batch_move_files(test_names, videoPath, test_dir_labels, 'txt')
+
+    shutil.move(train_dir, imagesDataDirectory) 
+    shutil.move(test_dir, imagesDataDirectory) 
+
+    shutil.move(train_dir_labels, labelsDataDirectory) 
+    shutil.move(test_dir_labels, labelsDataDirectory) 
+
+def foldersValidation(videoPath, filename, labelIndex):
+
+    if not os.path.exists(videoPath + f'/labels/test_{labelIndex}'):
+        os.makedirs(videoPath + f'/labels/test_{labelIndex}')
+
+    if not os.path.exists(videoPath + f'/labels/train_{labelIndex}'):
+        os.makedirs(videoPath + f'/labels/train_{labelIndex}')
 
     if not os.path.exists(videoPath):
         os.makedirs(videoPath)
 
-    if not os.path.exists(videoPath + '/images/test'):
-        os.makedirs(videoPath + '/images/test')
+    if not os.path.exists(videoPath + f'/images/test_{labelIndex}'):
+        os.makedirs(videoPath + f'/images/test_{labelIndex}')
 
-    if not os.path.exists(videoPath + '/images/train'):
-        os.makedirs(videoPath + '/images/train')
+    if not os.path.exists(videoPath + f'/images/train_{labelIndex}'):
+        os.makedirs(videoPath + f'/images/train_{labelIndex}')
 
-    test_dir = videoPath + '/images/test'
-    train_dir = videoPath + '/images/train'
+    pass
 
-    batch_move_files(train_names, videoPath, train_dir)
-    batch_move_files(test_names, videoPath, test_dir)
+def modifyYamlFile():
+    pass
